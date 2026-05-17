ff2b056185e9411d87d93275cd41705aa4813330
anaspop5132-commits <anaspop5132@gmail.com>
2026-05-18 01:16:06 +0300

    Create OwnerDashboard component for admin features

---
+++ client/app/owner/page.tsx
@@ -0,0 +1,37 @@
+export default function OwnerDashboard(){
+
+return(
+
+<div className="gradient min-h-screen p-10">
+
+<h1 className="text-5xl font-bold">
+لوحة تحكم الـ Owner
+</h1>
+
+<div className="grid lg:grid-cols-3 gap-6 mt-10">
+
+<div className="card p-8">
+<h2 className="text-2xl">
+إضافة Admin
+</h2>
+</div>
+
+<div className="card p-8">
+<h2 className="text-2xl">
+تغيير صور المنصة
+</h2>
+</div>
+
+<div className="card p-8">
+<h2 className="text-2xl">
+تعديل اسم المنصة
+</h2>
+</div>
+
+</div>
+
+</div>
+
+)
+
+}
